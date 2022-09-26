e6634d60f6fbb3b58fbce1e9839399c38430739a
Sanchit Bajaj <sanchit02@outlook.com>
2022-09-26 22:02:39 +0530

    added changes in js file

--- main.js
+++ main.js
@@ -3,3 +3,20 @@ console.log("hello world!");
 function main() {
   console.log("main code");
 }
+
+// adding new changes
+
+// add function to make a promise
+function makePromise(number) {
+  return new Promise((resolve, reject) => {
+    if (number % 2 === 0) {
+      resolve("number is even");
+    } else {
+      reject("number is odd");
+    }
+  });
+}
+
+makePromise(2)
+  .then((resp) => console.log(resp))
+  .catch((err) => console.log(err));
